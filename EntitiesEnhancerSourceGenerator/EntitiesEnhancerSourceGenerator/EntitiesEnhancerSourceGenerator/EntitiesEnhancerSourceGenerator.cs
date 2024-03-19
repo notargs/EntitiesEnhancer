cd52476ee721fbbcddbee8cefdc62060de3b15f9
notargs <satoyutaka94@gmail.com>
2024-03-19 23:21:26 +0900

    Rename namespaces

--- EntitiesEnhancerSourceGenerator/EntitiesEnhancerSourceGenerator/EntitiesEnhancerSourceGenerator/EntitiesEnhancerSourceGenerator.cs
+++ EntitiesEnhancerSourceGenerator/EntitiesEnhancerSourceGenerator/EntitiesEnhancerSourceGenerator/EntitiesEnhancerSourceGenerator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using EntityEnhancerSourceGenerator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
